738dd9e58b5817cb393b4ad598ce804695d3dc8c
Chau Dinh <chaudvb95uc@gmail.com>
2019-07-28 21:44:37 +0700

    graphql basic types. What i've noticed here is queries run in parallel and mutations run in order

--- src/index.js
+++ src/index.js
@@ -5,17 +5,63 @@ const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = gql`
   type Query {
     hello: String!
+    user: User!
+  }
+
+  type User {
+    id: Int!
+    username: String!
+  }
+
+  type Error {
+    field: String!
+    message: String!
+  }
+
+  type RegisterResponse {
+    user: User!
+    errors: [Error!]!
+  }
+
+  input UserInfo {
+    username: String!
+    password: String!
+    age: Int!
   }
 
   type Mutation {
-    register: User
+    register(userInfo: UserInfo!): RegisterResponse!
+    login(userInfo: UserInfo!): Boolean!
   }
 `;
 
 // Then, create a resolver
 const resolvers = {
   Query: {
-    hello: () => "Halo World!"
+    hello: () => "Halo World!",
+    user: () => ({
+      id: 2,
+      username: "Bob2"
+    })
+  },
+  Mutation: {
+    register: () => ({
+      user: {
+        id: 1,
+        username: "Bob"
+      },
+      errors: [
+        {
+          field: "username",
+          message: "bad!"
+        },
+        {
+          field: "username2",
+          message: "bad2!"
+        }
+      ]
+    }),
+    login: () => true
   }
 };
 
