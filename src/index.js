71081692f259a7763d3ca0416ac04999e5912d1b
Chau Dinh <chaudvb95uc@gmail.com>
2019-07-28 20:44:52 +0700

    Initial commit: graphql set up, query requests to apollo-server

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// The first part of the GraphQL is creating a schema or also known as typeDefs
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+// Then, create a resolver
+const resolvers = {
+  Query: {
+    hello: () => "Halo World!"
+  }
+};
+
+// Create an instance of Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log("server is listenning at ", url));
